2095f98b18ffd18d1b1130793abe2b7c02ecc5b2
Jhonatan Caldeira <jhonatansocaldeira@gmail.com>
2024-03-25 15:45:31 +0100

    GPT + Brave

--- brave_api_2.py
+++ brave_api_2.py
@@ -6,21 +6,22 @@ import gpt
 
 # Your Brave API key
 load_dotenv(dotenv_path='components/.env')
-api_key = os.getenv("brave_key")
 
-brave = Brave(api_key)
+def brave_request(query):
+    api_key = os.getenv("brave_key")
+    brave = Brave(api_key)
 
-query = "Quel temps il fait aujourd'hui?"
-num_results = 10
+    num_results = 10
 
-search_results = brave.search(q=query, count=num_results)
+    search_results = brave.search(q=query, count=num_results)
+    web_results = search_results.web_results
 
-web_results = search_results.web_results
+    brave_input = ''
+    for i in web_results:
+        brave_input += "\n" + i['description']
 
-brave_input = ''
-for i in web_results:
-    brave_input += "\n" + i['description']
+    return brave_input
 
-#print(brave_input)
-print(gpt.openai_request(query,''))
-print(gpt.openai_request(query,brave_input))
+    #print(brave_input)
+    print(gpt.openai_request(query,''))
+    print(gpt.openai_request(query,brave_input))
