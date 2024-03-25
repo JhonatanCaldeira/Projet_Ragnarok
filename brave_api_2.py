2659a343f39153ca149acd297a69c4190971af01
Jhonatan Caldeira <jhonatansocaldeira@gmail.com>
2024-03-25 15:40:16 +0100

    GPT + Brave

--- brave_api_2.py
+++ brave_api_2.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from brave import Brave
 import json 
+import gpt
 
 # Your Brave API key
 load_dotenv(dotenv_path='components/.env')
@@ -9,11 +10,17 @@ api_key = os.getenv("brave_key")
 
 brave = Brave(api_key)
 
-query = "comment prononcer json"
+query = "Quel temps il fait aujourd'hui?"
 num_results = 10
 
 search_results = brave.search(q=query, count=num_results)
 
 web_results = search_results.web_results
+
+brave_input = ''
 for i in web_results:
-    print(i['description'])
+    brave_input += "\n" + i['description']
+
+#print(brave_input)
+print(gpt.openai_request(query,''))
+print(gpt.openai_request(query,brave_input))
--- gpt.py
+++ gpt.py
@@ -4,17 +4,17 @@ import os
 
 load_dotenv(dotenv_path='components/.env')
 
-def openai_request(question):
+def openai_request(question, brave_input):
     client = OpenAI(api_key=os.environ.get("OPENAI_API_TOKEN"),)
+
+    gpt_input = "Answer this question: " + question + " taking in consideration this knowledge: " + brave_input
 
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "You are a genius of the web."},
-        {"role": "user", "content": question}
+        {"role": "user", "content": gpt_input}
     ]
     )
 
-    print(completion.choices[0].message)
-
-print(openai_request('qual a cor do mar'))
+    return completion.choices[0].message
