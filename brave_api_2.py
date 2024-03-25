7bc14e0be5859b8bcf7b6715d4c808dc68434392
Kevin Brunel <brunel.k.34@gmail.com>
2024-03-25 16:29:52 +0100

    mise en forme de la réponse

--- brave_api_2.py
+++ brave_api_2.py
@@ -1,3 +1,28 @@
+# import os
+# from dotenv import load_dotenv
+# from brave import Brave
+# import json 
+# import gpt
+
+# # Your Brave API key
+# load_dotenv(dotenv_path='components/.env')
+
+# def brave_request(query):
+#     api_key = os.getenv("brave_key")
+#     brave = Brave(api_key)
+
+#     num_results = 10
+
+#     search_results = brave.search(q=query, count=num_results)
+#     web_results = search_results.web_results
+
+#     brave_input = ''
+#     for i in web_results:
+#         brave_input += "\n" + i['description']
+
+#     return brave_input
+
+
 import os
 from dotenv import load_dotenv
 from brave import Brave
@@ -21,7 +46,3 @@ def brave_request(query):
         brave_input += "\n" + i['description']
 
     return brave_input
-
-    #print(brave_input)
-    print(gpt.openai_request(query,''))
-    print(gpt.openai_request(query,brave_input))
